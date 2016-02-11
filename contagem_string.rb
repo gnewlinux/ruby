c4a232e71da82f34880c50c39423eda9df3ce1bd
root <root@gnew>
2016-02-11 20:43:17 -0200

    Ruby é magico

---
+++ contagem_string.rb
@@ -0,0 +1,4 @@
+var1 = "Quantas caracteres existem nessa frase contando com os espaços?"
+var2 = var1.length
+
+puts var1 + var2.to_s
---
+++ reverse_string.rb
@@ -0,0 +1,6 @@
+var1 = "Testando de frente pra traz"
+var2 = "Testando de traz pra frente"
+
+puts var1
+puts var2.reverse
+
