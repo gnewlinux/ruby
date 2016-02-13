14bfd16ff5bca6f970b3a83d1d9919b22259ecfd
gnewlinux <gnewlinux@gmail.com>
2016-02-13 16:19:22 -0200

    Variavel Local

---
+++ cadaa_array.rb
@@ -0,0 +1,10 @@
+
+linguagens = ['Portugues','Ingles','Ruby']
+
+linguagens.each do |ling|
+	puts 'Eu adoro ' + ling + '!'
+	puts 'Voce nao?'
+end
+
+puts 'E vamos ouvir sobre C++'
+puts '...'
---
+++ variavel_local.rb
@@ -0,0 +1,6 @@
+def dobreNumero num
+	numVezes2 = num * 2
+	puts 'O dobro de '+ num.to_s  + ' é ' + numVezes2.to_s
+end
+
+dobreNumero 44
