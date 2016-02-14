65680d19e1a95fe0e0cf51d58ffff12c207ec2fa
root <root@gnew>
2016-02-14 17:41:44 -0200

    aleatorio

---
+++ aleatorio.rb
@@ -0,0 +1,16 @@
+puts rand
+puts rand
+puts rand.to_i
+puts (rand(100))
+
+puts ""
+srand 1776
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ""
+srand 1776
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+
